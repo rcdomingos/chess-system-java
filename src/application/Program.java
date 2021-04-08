0fd756f118051e3097cf7ab77a99249c33f8b74a
Reginaldo C Domingos <rcdomingos@live.com>
2021-04-08 13:23:30 -0300

    add chess layer and printing the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,13 @@
 package application;
 
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println("checkmate!");
 
-        Position position = new Position(1,2);
-        System.out.println(position);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+
     }
 }

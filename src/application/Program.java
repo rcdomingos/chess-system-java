f8608a521ad31adfda3c35c9d8f58db9621c70d4
Reginaldo C Domingos <rcdomingos@live.com>
2021-04-08 10:46:26 -0300

    add first class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
         System.out.println("checkmate!");
+
+        Position position = new Position(1,2);
+        System.out.println(position);
     }
 }
